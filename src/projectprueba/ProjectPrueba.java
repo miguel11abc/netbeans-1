718261f69588f58b2f375cbb2837b62e9f22b50a
Isabel-Puertas9 <79533578+Isabel-Puertas9@users.noreply.github.com>
2021-03-06 16:37:38 +0100

    Update ProjectPrueba.java

--- src/projectprueba/ProjectPrueba.java
+++ src/projectprueba/ProjectPrueba.java
@@ -17,6 +17,7 @@ public class ProjectPrueba {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        System.out.println("Adiós");
     }
     
 }
